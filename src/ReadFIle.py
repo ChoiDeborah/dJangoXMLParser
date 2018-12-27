a6eedf3885b4b87ffd5b077ddc30a4c8ec3cbd64
ChoiDeborah <adorablejimini@gmail.com>
2018-12-27 16:41:51 +0900

    Load All Sentences

--- src/ReadFIle.py
+++ src/ReadFIle.py
@@ -1,33 +1,50 @@
 # -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
+from typing import Any, Union
 
 path = "/Users/deborah/Desktop/dJangoXMLParser/example/Example_Sentences_181216.xml"
+global count
+global totalCount
+totalCount = 0
+
+def Load_XML(pathName):
+    f = open(pathName, 'r')
+    string = ""
+    isline = False
+    count = 0
 
 
-f = open(path, 'r')
-string = ""
-isline = False
+    while True:
+        line = f.readline()
+        if not line:
+            if not isline:
+                break
 
-while True:
-    line = f.readline()
-    if not line:
-        break
-    if not line == '\n' and not line[:4] == '<!--':
-        string += line
-        isline = True
-    else:
-        if isline:
-            isline = False
 
-    if not isline:
-        if not string == "":
+
+        if not line == '\n' and not line[:4] == '<!--' and not line == '':
+            string += line
+            isline = True
+        else:
+            if isline:
+                isline = False
+
+        if not isline and not string == "":
             root = ET.fromstring(string)
+            print(count+1)
             print(root.tag)
+
             string = ""
+            count += 1
             isline = False
 
-f.close()
+    f.close()
+    return count
 
 
-# for child in root:
-#    print child.tag, child.attrib
+for i in range(1, 13):
+
+    print(i)
+    totalCount += Load_XML("/Users/deborah/Desktop/dJangoXMLParser/xml/"+str(i)+".xml")
+
+    print(totalCount)
