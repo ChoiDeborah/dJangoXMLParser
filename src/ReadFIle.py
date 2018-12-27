fd8a3f27eb88a9f8dcba4d6c113c863d34f33344
ChoiDeborah <adorablejimini@gmail.com>
2018-12-27 12:49:16 +0900

    Modify Reflection - Mulan of Disney.xml

--- src/ReadFIle.py
+++ src/ReadFIle.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
 
-path = "/Users/deborah/Desktop/dJangoXMLParser/xml/Oscar Wilde Quotes - Oscar Wilde.xml"
+path = "/Users/deborah/Desktop/dJangoXMLParser/xml/Reflection - Mulan of Disney.xml"
 
 
 f = open(path, 'r')
